2801dfc13690068606f8bb7c4d7f9e533716e8c4
AdnanSB <56301144+AdnanSB@users.noreply.github.com>
2020-05-08 14:24:18 +0200

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -130,7 +130,7 @@
 
                   <p>
                     I et af vores andre 2. semesters opgaver, skulle vi lave en
-                    reklamefilm til CMS, som skulle illustere hvad de kan lære
+                    reklamefilm til CMS, som skulle illustrere hvad de kan lære
                     på kurset. Samt hvordan de skulle håndtere kritiske
                     situationer, når de er ude på havet.
                   </p>
